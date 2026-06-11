bc60e7689807413f89450cf8a9700a313ae4da2f
Jinx <jinxynet@duck.com>
2026-06-11 12:53:28 +0200

    first commit

---
+++ backend/src/server.ts
@@ -0,0 +1,23 @@
+import express, {
+  type Request,
+  type Response,
+  type Express,
+  type NextFunction,
+} from "express";
+
+const app: Express = express();
+const port = 3000;
+const loggerMiddleware = (req: Request, res: Response, next: NextFunction) => {
+  console.log(`Received a ${req.method} request to ${req.url}`);
+  next();
+};
+
+app.use(loggerMiddleware);
+
+app.get("/", (req: Request, res: Response) => {
+  res.status(200).json({ status: "ok" });
+});
+
+app.listen(port, () => {
+  console.log(`Server is running in port ${port}`);
+});
